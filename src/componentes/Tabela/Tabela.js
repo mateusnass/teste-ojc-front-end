37d149be7cc129a690b1cdf17a49a8c195e156e3
Mateus Nass <matenass@hotmail.com>
2023-01-26 03:41:16 -0300

    Criação de erro 404

--- src/componentes/Tabela/Tabela.js
+++ src/componentes/Tabela/Tabela.js
@@ -1,32 +1,57 @@
-function Tabela({botao}){
-  return(
-    <div>
+import "./Tabela.css";
+
+function Tabela({ botao }) {
+  return (
+    <div className="container">
       <table className="table">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Data Cadastro</th>
-          <th>Nome / Razão Social</th>
-          <th>Cpf / Cnpj</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
+        <tbody>
+          <tr>
+            <th>Id</th>
+            <th>Data Cadastro</th>
+            <th>Nome / Razão Social</th>
+            <th>Cpf / Cnpj</th>
+            <th>Ações</th>
+          </tr>
 
-      <tbody>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td className="botoes">
+              <div className="botaoAlterar">
+                <button className="buttonAlt">Alterar</button>
+                <button className="buttonExcl" type="submit">
+                  <a href="" className="buttonAltA">
+                    {" "}
+                    Excluir{" "}
+                  </a>
+                </button>
+              </div>
+            </td>
+          </tr>
 
-      </tbody>
-
-    </table>
-  </div>
-)
-
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <div className="botaoAlterar">
+                <button className="buttonAlt">Alterar</button>
+                <button className="buttonExcl" type="submit">
+                  <a href="" className="buttonAltA">
+                    {" "}
+                    Excluir{" "}
+                  </a>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Tabela;
